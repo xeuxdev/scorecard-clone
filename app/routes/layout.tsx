085f464c3db2b3330d71afec54942bc55f751ca6
xeuxdev <tochukwujohn24@gmail.com>
2025-10-25 05:42:46 +0100

    rm gsap imports

--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -1,19 +1,8 @@
-import { useEffect } from "react";
 import { Outlet } from "react-router";
 import { GlobalEmbed } from "~/components/global-embed";
 import { Navbar } from "~/components/navbar";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function PageLayout() {
-  useEffect(() => {
-    // Initialize GSAP
-    gsap.registerPlugin(ScrollTrigger);
-
-    // Optional: Set default easing or other global settings
-    gsap.defaults({ duration: 0.3, ease: "power2.inOut" });
-  }, []);
-
   return (
     <div className="page-wrapper">
       <GlobalEmbed />
